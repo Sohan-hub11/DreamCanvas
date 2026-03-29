6fa17c9e7807c1af026f7cfbdfa2d532164eb916
Sohan Samanta <sohansamanta18@gmail.com>
2026-03-30 03:11:15 +0530

    v1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
 
 const App = () => {
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy-credit" element={<BuyCredit />} />
+      </Routes>
     </div>
   )
 }
